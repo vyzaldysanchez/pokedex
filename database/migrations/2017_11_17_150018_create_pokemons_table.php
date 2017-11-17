44eea6b3df3490eb375f6b78043599182f2411ef
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-17 11:24:46 -0400

    Renames the pokemons table correctly

--- database/migrations/2017_11_17_150018_create_pokemons_table.php
+++ database/migrations/2017_11_17_150018_create_pokemons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePokemonTable extends Migration
+class CreatePokemonsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePokemonTable extends Migration
      */
     public function up()
     {
-        Schema::create('pokemon', function (Blueprint $table) {
+        Schema::create('pokemons', function (Blueprint $table) {
             $table->increments('id');
             $table->text('description');
             $table->boolean('is_private');
@@ -32,6 +32,6 @@ class CreatePokemonTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pokemon');
+        Schema::dropIfExists('pokemons');
     }
 }
