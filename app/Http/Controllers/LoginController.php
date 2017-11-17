b01a47a3209208f03c253fb5af1c6580d2af8448
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-17 16:57:24 -0400

    Adds allows-guest validation to Login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
     public function index()
     {
         return view('auth.login');
