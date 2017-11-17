b1555efe444a77688cf95d3a605008c02b8f8f97
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-17 16:29:50 -0400

    Creates form component for user registration

---
+++ resources/assets/js/components/auth/registration/RegistrationForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Grid, Cell, TextField, Button } from 'react-md';
+
+class RegistrationForm extends Component {
+  render() {
+    return (
+      <div>
+        <Grid className="registration-form">
+          <Cell size={8} desktopOffset={2}>
+            <h1 className="text-center md-text--theme-primary no-margin-top">
+              Create an account
+            </h1>
+
+            <form>
+              <Cell size={6}>
+                <TextField
+                  id="fullName"
+                  label="Your full name here!"
+                  lineDirection="right"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={6}>
+                <TextField
+                  id="tel"
+                  type="tel"
+                  label="Your telephone here!"
+                  lineDirection="left"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={6}>
+                <TextField
+                  id="email"
+                  type="email"
+                  label="Your email!"
+                  lineDirection="left"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={6}>
+                <TextField
+                  id="username"
+                  label="Your username!"
+                  lineDirection="right"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={6}>
+                <TextField
+                  id="password"
+                  type="password"
+                  label="Your password here!"
+                  lineDirection="left"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={6}>
+                <TextField
+                  id="password_confirmation"
+                  type="password"
+                  label="Confirm your password here!"
+                  lineDirection="left"
+                  className="md-cell--bottom"
+                />
+              </Cell>
+              <Cell size={12}>
+                <Button raised primary>
+                  Create account
+                </Button>
+                <a href="/login" className="pull-right">
+                  Already have an account? Log in
+                </a>
+              </Cell>
+            </form>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+const formContainer = document.getElementById('pokedex-user-registration');
+
+if (formContainer) {
+  ReactDOM.render(<RegistrationForm />, formContainer);
+}
