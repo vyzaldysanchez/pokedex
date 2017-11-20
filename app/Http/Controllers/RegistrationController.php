3c82e65b1f07b31609ffcda881ee9d7804cec493
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-20 15:38:41 -0400

    Registers user via request

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\RegistrationRequest;
+use App\User;
 
 class RegistrationController extends Controller
 {
@@ -19,6 +20,16 @@ class RegistrationController extends Controller
   public function register(RegistrationRequest $request)
   {
     $request->validate();
+
+    $user = User::create([
+      'username' => $request->get('username'),
+      'full_name' => $request->get('fullName'),
+      'city' => $request->get('city'),
+      'email' => $request->get('email'),
+      'password' => bcrypt($request->get('password')),
+    ]);
+
+    \Auth::login($user);
 
     return redirect('/');
   }
