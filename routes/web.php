a09ea40246353faf14b1aa8c2ffe83b532fa6d3f
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-20 16:11:34 -0400

    Log users out

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 Route::get('/', 'HomeController@index');
+Route::redirect('/home', '/');
 Route::get('/login', 'LoginController@index')->name('login');
+Route::get('/logout', 'LoginController@logout');
 Route::get('/register', 'RegistrationController@index');
 Route::post('/register', 'RegistrationController@register');
