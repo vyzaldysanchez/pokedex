3b41c0792b9170bb74d75f7dcdd0323fc1943e38
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-21 09:35:40 -0400

    Refactors auth controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,6 +1,7 @@
 <?php
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\LoginRequest;
 
