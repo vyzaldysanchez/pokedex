365e389bd2bc953d7a261d877340e6a6455d1e5e
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-21 09:25:01 -0400

    Allows user login via HTTP request

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+
+class LoginController extends Controller
+{
+  protected $redirectTo = '/';
+
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+
+  public function index()
+  {
+    return view('auth.login');
+  }
+
+  public function login(LoginRequest $request)
+  {
+    $request->validate();
+
+    if (\Auth::attempt($request->all())) {
+      return redirect('/');
+    }
+  }
+
+  public function logout()
+  {
+    \Auth::logout();
+
+    return redirect('/login');
+  }
+}
---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+Route::get('/', 'HomeController@index');
+Route::redirect('/home', '/');
+Route::get('/login', 'LoginController@index')->name('login');
+Route::post('/login', 'LoginController@login');
+Route::get('/logout', 'LoginController@logout');
+Route::get('/register', 'RegistrationController@index');
+Route::post('/register', 'RegistrationController@register');
