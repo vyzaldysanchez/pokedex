550f8c1fc15036e1048651564b332b147bb7dfb2
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-22 12:23:08 -0400

    Groups routes by middleware

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('guest');
-    }
-
     public function index()
     {
         return view('auth.register');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
-Route::get('/', 'HomeController@index');
-Route::get('/login', 'LoginController@index')->name('login');
-Route::view('/register', 'auth.register');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', 'HomeController@index');
+});
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', 'LoginController@index')->name('login');
+    Route::view('/register', 'auth.register');
+});
