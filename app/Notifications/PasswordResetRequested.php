c38b3a9257a1f50ab8bbe51b4948be05cacbc9a1
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-23 15:16:54 -0400

    Uses blade template to define password reset emails

--- app/Notifications/PasswordResetRequested.php
+++ app/Notifications/PasswordResetRequested.php
@@ -8,55 +8,60 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 class PasswordResetRequested extends Notification
 {
-  use Queueable;
+    use Queueable;
 
-  protected $token;
+    protected $token;
 
-  protected $email;
+    protected $email;
 
-  public function __construct(string $token, string $email)
-  {
-    $this->token = $token;
-    $this->email = $email;
-  }
+    public function __construct(string $token, string $email)
+    {
+        $this->token = $token;
+        $this->email = $email;
+    }
 
-  /**
-   * Get the notification's delivery channels.
-   *
-   * @param  mixed  $notifiable
-   * @return array
-   */
-  public function via($notifiable)
-  {
-    return ['mail'];
-  }
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
 
-  /**
-   * Get the mail representation of the notification.
-   *
-   * @param  mixed  $notifiable
-   * @return \Illuminate\Notifications\Messages\MailMessage
-   */
-  public function toMail($notifiable)
-  {
-    return (new MailMessage)
-      ->line('You are receiving this email because you requested to change your password.')
-      ->line('In case you are not the one who requested this, ignore this message, otherwise:')
-      ->action('Reset your password here!', url('/password/reset/' . $this->token . '/?email=' . $this->email))
-      ->line('Thank you for using your Pokedex!');
-  }
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+        ->subject('Password reset requested')
+        ->markdown(
+            'mails.password.reset',
+            [
+                'token' => $this->token,
+                'email' => $this->email,
+                'url' => url('/password/reset/' . $this->token . '/?email=' . $this->email)
+            ]
+        );
+    }
 
-  /**
-   * Get the array representation of the notification.
-   *
-   * @param  mixed  $notifiable
-   * @return array
-   */
-  public function toArray($notifiable)
-  {
-    return [
-      'token' => $this->token,
-      'email' => $this->email
-    ];
-  }
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+        'token' => $this->token,
+        'email' => $this->email
+        ];
+    }
 }
