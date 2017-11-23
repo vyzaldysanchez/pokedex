ef89205f1966c2f95c2b4499a68fa220f89f5d30
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-23 16:32:46 -0400

    Creates PokedexHeader component

--- resources/assets/js/components/pokedex/Pokedex.js
+++ resources/assets/js/components/pokedex/Pokedex.js
@@ -1,42 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Toolbar } from 'react-md';
-import PokedexNav from './PokedexNav';
-import PokedexAccountBox from './account-box/PokedexAccountBox';
-import { styles } from './styles-vars';
+import { PokedexHeader } from './PokedexHeader';
 
 export class Pokedex extends Component {
 	constructor(props) {
 		super(props);
-
-		this.state = { accountBoxCollapsed: true };
-		this.toggleAccountBox = this.toggleAccountBox.bind(this);
-	}
-
-	toggleAccountBox() {
-		this.setState({ accountBoxCollapsed: !this.state.accountBoxCollapsed });
 	}
 
 	render() {
-		return (
-			<div>
-				<Toolbar
-					id="pokemon-toolbar"
-					fixed
-					colored
-					title="Pokedex"
-					titleStyle={{ fontWeight: 900 }}
-					style={{ ...styles }}
-					actions={
-						<PokedexNav
-							className="md-paper--2"
-							onClick={this.toggleAccountBox}
-						/>
-					}
-				/>
-				<PokedexAccountBox collapsed={this.state.accountBoxCollapsed} />
-			</div>
-		);
+		return <PokedexHeader />;
 	}
 }
 
---
+++ resources/assets/js/components/pokedex/PokedexHeader.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Toolbar } from 'react-md';
+import PokedexNav from './PokedexNav';
+import PokedexAccountBox from './account-box/PokedexAccountBox';
+import { styles } from './styles-vars';
+
+export class PokedexHeader extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { accountBoxCollapsed: true };
+		this.toggleAccountBox = this.toggleAccountBox.bind(this);
+	}
+
+	toggleAccountBox() {
+		this.setState({ accountBoxCollapsed: !this.state.accountBoxCollapsed });
+	}
+
+	render() {
+		return (
+			<div className="pokedex-header">
+				<Toolbar
+					id="pokemon-toolbar"
+					fixed
+					colored
+					title="Pokedex"
+					titleStyle={{ fontWeight: 900 }}
+					style={{ ...styles }}
+					actions={
+						<PokedexNav
+							className="md-paper--2"
+							onClick={this.toggleAccountBox}
+						/>
+					}
+				/>
+				<PokedexAccountBox collapsed={this.state.accountBoxCollapsed} />
+			</div>
+		);
+	}
+}
