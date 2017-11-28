771211c22a7e5e19bfab6328bab558558068d965
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-28 13:18:57 -0400

    Refactors component

--- resources/assets/js/components/pokedex/pokemons/AddPokemon.js
+++ resources/assets/js/components/pokedex/pokemons/AddPokemon.js
@@ -8,7 +8,22 @@ export class AddPokemon extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { numberMin: 1, fileName: '', pokemonTypes: [] };
+		this.state = {
+			numberMin: 1,
+			pokemonTypes: [],
+			pokemon: {
+				name: '',
+				typesIds: [],
+				age: 0,
+				pounds: 0.0,
+				captured: false,
+				public: false,
+				image: '',
+				imageName: '',
+				description: ''
+			}
+		};
+		this.handleTypeSelection = this.handleTypeSelection.bind(this);
 	}
 
 	componentDidMount() {
@@ -17,13 +32,35 @@ export class AddPokemon extends Component {
 			.then(pokemonTypes => this.setState({ pokemonTypes }));
 	}
 
+	handleTypeSelection(id) {
+		let typesIds = this.state.pokemon.typesIds.slice();
+		const typeSelectedIndex = typesIds.indexOf(id);
+
+		if (typeSelectedIndex > -1) {
+			typesIds = [
+				...typesIds.slice(0, typeSelectedIndex),
+				...typesIds.slice(typeSelectedIndex + 1)
+			];
+		} else {
+			typesIds.push(id);
+		}
+
+		const pokemon = { ...this.state.pokemon, typesIds };
+
+		this.setState({ pokemon: { ...this.state.pokemon, typesIds } });
+	}
+
 	render() {
 		return (
 			<Grid>
 				<Cell size={EIGHT_COLUMNS}>
 					<h1>Add a Pokemon</h1>
 
-					<AddPokemonForm pokemonTypes={this.state.pokemonTypes} />
+					<AddPokemonForm
+						pokemonTypes={this.state.pokemonTypes}
+						pokemon={this.state.pokemon}
+						onTypeSelection={this.handleTypeSelection}
+					/>
 				</Cell>
 			</Grid>
 		);
--- resources/assets/js/components/pokedex/pokemons/AddPokemonForm.js
+++ resources/assets/js/components/pokedex/pokemons/AddPokemonForm.js
@@ -13,8 +13,9 @@ import {
 	FIVE_COLUMNS,
 	TWELVE_COLUMNS
 } from '@pokedex/assets/js/utils/ui-columns';
+import { RED } from '@pokedex/assets/js/utils/colors';
 
-const renderPokemonTypeSelection = (type, index) => (
+const renderPokemonTypeSelection = (onChange, type, index) => (
 	<SelectionControl
 		id={`pokemon-types-checkbox-${index}`}
 		key={index}
@@ -25,103 +26,135 @@ const renderPokemonTypeSelection = (type, index) => (
 			<FontIcon style={{ color: type.color }}>check</FontIcon>
 		}
 		inline={true}
+		onChange={() => onChange(type.id)}
 	/>
 );
 
-const AddPokemonForm = props => (
-	<form className="add-pokemon-form" onSubmit={props.onSubmit}>
-		<input type="hidden" name="_token" value={props.csrfToken} />
+const POKEMON_AGE_MIN = 1;
+const POKEMON_POUNDS_MIN = 0.01;
 
-		<TextField
-			name="name"
-			id="pokemon-name"
-			label="The pokemon name here"
-			required
-		/>
+const AddPokemonForm = props => {
+	const {
+		csrfToken,
+		pokemonTypes,
+		pokemon,
+		onSubmit,
+		onTypeSelection,
+		onImageSelected
+	} = props;
 
-		<Cell size={TWELVE_COLUMNS}>
-			<label style={{ display: 'block' }}>
-				Select the types of this pokemon:
-			</label>
-			{props.pokemonTypes.map(renderPokemonTypeSelection)}
-		</Cell>
-
-		<Cell size={SIX_COLUMNS}>
-			<TextField
-				name="age"
-				id="pokemon-age"
-				label="The pokemon age in years"
-				type="number"
-				min={props.numberMin}
-				required
-			/>
-		</Cell>
-
-		<Cell size={SIX_COLUMNS}>
-			<TextField
-				name="pounds"
-				id="pokemon-pounds"
-				label="The pokemon pounds"
-				type="number"
-				min={props.numberMin}
-				required
-			/>
-		</Cell>
-
-		<SelectionControl
-			id="pokemon-captured-checkbox"
-			name="captured"
-			type="checkbox"
-			label="Captured?"
-			checkedCheckboxIcon={<FontIcon>check</FontIcon>}
-			inline={true}
-		/>
-
-		<SelectionControl
-			id="pokemon-is-public-checkbox"
-			name="public"
-			type="checkbox"
-			label="Public?"
-			checkedCheckboxIcon={<FontIcon>check</FontIcon>}
-			inline={true}
-		/>
-
-		<Cell size={TWELVE_COLUMNS}>
-			<FileUpload
-				id="pokemon-image"
-				label="Choose an image"
-				required
-				accept="image/*"
-				name="image"
-				className="file-inputs__upload-form__file-upload"
-				primary
-				iconBefore
-				onLoad={({ name }) => this.setState({ fileName: name })}
-			/>
+	return (
+		<form className="add-pokemon-form" onSubmit={onSubmit}>
+			<input type="hidden" name="_token" value={csrfToken} />
 
 			<TextField
-				id="pokemon-image-name-field"
-				placeholder="No image selected"
-				value={props.fileName}
-				className="file-inputs__upload-form__file-field"
-				readOnly
-				fullWidth={false}
+				name="name"
+				id="pokemon-name"
+				label="The pokemon name here"
+				value={pokemon.name}
+				required
 			/>
-		</Cell>
 
-		<TextField
-			label="Provide a description"
-			id="pokemon-description"
-			name="description"
-			rows={FIVE_COLUMNS}
-		/>
+			<Cell size={TWELVE_COLUMNS}>
+				<label style={{ display: 'block' }}>
+					Select the types of this pokemon:
+				</label>
+				{pokemonTypes.map(
+					renderPokemonTypeSelection.bind(null, onTypeSelection)
+				)}
+			</Cell>
 
-		<Cell size={TWELVE_COLUMNS}>
-			<Button flat secondary swapTheming>
-				Add To Pokedex
-			</Button>
-		</Cell>
-	</form>
-);
+			<Cell size={SIX_COLUMNS}>
+				<TextField
+					name="age"
+					id="pokemon-age"
+					label="The pokemon age in years"
+					type="number"
+					min={POKEMON_AGE_MIN}
+					value={pokemon.age}
+					required
+				/>
+			</Cell>
+
+			<Cell size={SIX_COLUMNS}>
+				<TextField
+					name="pounds"
+					id="pokemon-pounds"
+					label="The pokemon pounds"
+					type="number"
+					min={POKEMON_POUNDS_MIN}
+					value={pokemon.pounds}
+					required
+				/>
+			</Cell>
+
+			<SelectionControl
+				id="pokemon-captured-checkbox"
+				name="captured"
+				type="checkbox"
+				label="Captured?"
+				checkedCheckboxIcon={<FontIcon>check</FontIcon>}
+				inline={true}
+				onChange={null}
+				checked={pokemon.captured}
+			/>
+
+			<SelectionControl
+				id="pokemon-is-public-checkbox"
+				name="public"
+				type="checkbox"
+				label="Public?"
+				checkedCheckboxIcon={<FontIcon>check</FontIcon>}
+				inline={true}
+				onChange={null}
+				checked={pokemon.public}
+			/>
+
+			<Cell size={TWELVE_COLUMNS}>
+				<FileUpload
+					id="pokemon-image"
+					label="Choose an image"
+					required
+					accept="image/*"
+					name="image"
+					className="file-inputs__upload-form__file-upload"
+					primary
+					iconBefore
+					onLoad={onImageSelected}
+				/>
+
+				<TextField
+					id="pokemon-image-name-field"
+					placeholder="No image selected"
+					className="file-inputs__upload-form__file-field"
+					readOnly
+					fullWidth={false}
+					value={pokemon.imageName}
+				/>
+			</Cell>
+
+			<TextField
+				label="Provide a description"
+				id="pokemon-description"
+				name="description"
+				rows={FIVE_COLUMNS}
+				value={pokemon.description}
+			/>
+
+			<Cell size={TWELVE_COLUMNS}>
+				<Button
+					flat
+					secondary
+					swapTheming
+					style={{ backgroundColor: RED }}
+					iconBefore
+					iconEl={<FontIcon>add</FontIcon>}
+				>
+					Add To Pokedex
+				</Button>
+			</Cell>
+		</form>
+	);
+};
 
 export { AddPokemonForm };
