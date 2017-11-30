e8e69e26ac1de821f03d3e75a6e316e59042fd44
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-11-30 15:28:12 -0400

    Triggers search action

--- resources/assets/js/components/pokedex/body/PokedexSearch.js
+++ resources/assets/js/components/pokedex/body/PokedexSearch.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { FontIcon, TextField } from 'react-md';
+import { Button, Cell, FontIcon, Grid, TextField } from 'react-md';
 import axios from 'axios';
 import PokedexTypesFilter from './PokedexTypesFilter';
 import { connect } from 'react-redux';
@@ -7,39 +7,66 @@ import {
 	ADD_POKEMON_TYPE_FILTER,
 	ADD_SEARCH_TEXT_FILTER
 } from '@pokedex/assets/js/components/pokedex/actions';
+import { TWELVE_COLUMNS } from '@pokedex/assets/js/utils/ui-columns';
 
 export class PokedexSearch extends Component {
 	constructor(props) {
 		super(props);
 
-		this.search = this.search.bind(this);
-		this.searchByPokemonType = this.searchByPokemonType.bind(this);
+		this.addSearchTextFilter = this.addSearchTextFilter.bind(this);
+		this.addPokemonTypeFilter = this.addPokemonTypeFilter.bind(this);
+		this.triggerSearch = this.triggerSearch.bind(this);
 	}
 
-	search(text) {
+	addSearchTextFilter(text) {
 		this.props.dispatch({ type: ADD_SEARCH_TEXT_FILTER, payload: text });
 	}
 
-	searchByPokemonType(type) {
+	addPokemonTypeFilter(type) {
 		this.props.dispatch({ type: ADD_POKEMON_TYPE_FILTER, payload: type });
+	}
+
+	triggerSearch() {
+		this.props.dispatch({ type: SEARCH });
+	}
+
+	renderSearchIcon() {
+		return <FontIcon>search</FontIcon>;
 	}
 
 	render() {
 		return (
-			<div className="pokedex-search">
-				<TextField
-					id="pokedex-search-box"
-					type="search"
-					leftIcon={<FontIcon>search</FontIcon>}
-					label="Search by name/description..."
-					value={this.props.search}
-					onChange={this.search}
-				/>
+			<Grid className="pokedex-search">
+				<Cell size={TWELVE_COLUMNS}>
+					<TextField
+						id="pokedex-search-box"
+						type="search"
+						leftIcon={this.renderSearchIcon()}
+						label="Search by name/description..."
+						value={this.props.search}
+						onChange={this.addSearchTextFilter}
+					/>
+				</Cell>
 
-				<PokedexTypesFilter
-					onPokemonTypeSelected={this.searchByPokemonType}
-				/>
-			</div>
+				<Cell size={TWELVE_COLUMNS}>
+					<PokedexTypesFilter
+						onPokemonTypeSelected={this.addPokemonTypeFilter}
+					/>
+				</Cell>
+
+				<Cell size={TWELVE_COLUMNS}>
+					<Button
+						raised
+						primary
+						iconBefore
+						iconEl={this.renderSearchIcon()}
+						className="center-block"
+						onClick={this.triggerSearch}
+					>
+						Search
+					</Button>
+				</Cell>
+			</Grid>
 		);
 	}
 }
