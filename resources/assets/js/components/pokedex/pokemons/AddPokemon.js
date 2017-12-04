ee6da0bcf902992843986020aaf86501d6c953f1
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-12-04 11:46:31 -0400

    Allows edit form to send PUT requests

--- resources/assets/js/components/pokedex/pokemons/AddPokemon.js
+++ resources/assets/js/components/pokedex/pokemons/AddPokemon.js
@@ -28,7 +28,7 @@ class AddPokemon extends BaseFormContainer {
 
 		this.state = {
 			pokemon: this.generatePokemonFields({}),
-			pokemonImage: null
+			pokemonImage: ''
 		};
 		this.handleTypeSelection = this.handleTypeSelection.bind(this);
 		this.handleInput = this.handleInput.bind(this);
@@ -65,15 +65,11 @@ class AddPokemon extends BaseFormContainer {
 						pounds: Number.parseFloat(pokemon.pounds),
 						description: pokemon.description,
 						captured: pokemon.captured,
-						public: pokemon.public
+						isPublic: pokemon.public
 					})
 				)
 			});
 		});
-	}
-
-	get submitFunc() {
-		return this.props.edit ? axios.put : axios.post;
 	}
 
 	get submitUrl() {
@@ -136,7 +132,7 @@ class AddPokemon extends BaseFormContainer {
 					description: this.state.pokemon.description.value,
 					image: this.state.pokemon.image.value,
 					captured: this.state.pokemon.captured.value,
-					public: this.state.pokemon.public.value
+					isPublic: this.state.pokemon.public.value
 				})
 			)
 		});
@@ -145,6 +141,10 @@ class AddPokemon extends BaseFormContainer {
 	submitForm() {
 		const formData = new FormData();
 		const pokemon = this.state.pokemon;
+
+		if (this.props.edit) {
+			formData.append('_method', 'PUT');
+		}
 
 		formData.append('name', pokemon.name.value);
 		formData.append('age', Number.parseFloat(pokemon.age.value));
@@ -163,7 +163,8 @@ class AddPokemon extends BaseFormContainer {
 
 		domUtils.disableElements(formElements);
 
-		this.submitFunc(this.submitUrl, formData)
+		axios
+			.post(this.submitUrl, formData)
 			.then(res => {
 				sendNotificationMessage(
 					this.props.dispatch,
