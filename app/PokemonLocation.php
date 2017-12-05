1b656bbf20b8c259b849c79e208b439da1e434d7
Vyzaldy Sanchez <v.sanchez@gbh.com.do>
2017-12-05 14:54:08 -0400

    Defines Pokemon -> PokemonLocation relation

--- app/PokemonLocation.php
+++ app/PokemonLocation.php
@@ -4,14 +4,28 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class PokemonLocation
  * @package App
+ *
+ * @property float $latitude
+ * @property float $longitude
  */
 class PokemonLocation extends Model
 {
     use SoftDeletes;
 
     protected $dates = ['deleted_at'];
+
+    protected $fillable = ['latitude', 'longitude', 'pokemon_id'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function pokemon(): BelongsTo
+    {
+        return $this->belongsTo(Pokemon::class);
+    }
 }
